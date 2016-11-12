f89cc54e28c4fb351050e704333564d54040952f
Bruce Nguyen <brucenguyen@Bruces-MacBook-Pro.local>
2016-11-12 15:58:48 -0500

    fixed labeling data

--- test-w-images.py
+++ test-w-images.py
@@ -15,7 +15,9 @@ import numpy as np
 
 
 filename_queue = tf.train.string_input_producer(
-    tf.train.match_filenames_once("undercooked/*.jpg"))
+    tf.train.match_filenames_once('undercooked/*.jpg'))
+
+fi = tf.train.match_filenames_once('undercooked/*.jpg')
 
 # filenames = ['undercooked/green.jpg', 'undercooked/blue.jpg']
 # filename_queue = tf.train.string_input_producer(filenames)
@@ -28,56 +30,49 @@ image_reader = tf.WholeFileReader()
 # Read a whole file from the queue, the first returned value in the tuple is the
 # filename which we are ignoring.
 key, value = image_reader.read(filename_queue)
+# print value
+
 
 
 # Decode the image as a JPEG file, this will turn it into a Tensor which we can
 # then use in training.
-my_image = tf.image.decode_jpeg(value, channels=1)
-all_imgs = []
+# my_image = tf.image.decode_jpeg(value, channels=1)
+data = []
+lab = []
 
+seen = set()
 # Start a new session to show example output.
-with tf.Session() as sess:
+with tf.Session() as sess:  
     # Required to get the filename matching to run.
     tf.initialize_all_variables().run()
 
     # Coordinate the loading of image files.
     coord = tf.train.Coordinator()
     threads = tf.train.start_queue_runners(coord=coord)
-
-    for i in range(20):
-        image = my_image.eval()
-        # print len(image)
-        image = tf.image.resize_images(image, 300, 300)
-        leh = tf.reshape(image, [1, 90000])
-        all_imgs.append(leh.eval())   
     
-
-    # Finish off the filename queue coordinator.
+    for i in range(20):
+        file_name = key.eval()
+        labels = file_name.split('-')
+        my_img =  tf.image.decode_jpeg(tf.read_file(file_name), channels=1)
+        image = tf.image.resize_images(my_img, 300, 300)
+        leh = tf.reshape(image, [1, 90000])
+        if labels[1] == 'AM':
+            lab.append(1)
+        else:
+            lab.append(0)
+        data.append(leh.eval()[0])
     coord.request_stop()
     coord.join(threads)
-# print all_imgs
-# print len(all_imgs)
-# for i in all_imgs:
-    # print i
-# labels1 = [1]*10
-# labels2 = [0]*10
-# labels  =labels1+labels2
-# print labels
+  
 
 
-
-# In[10]:
 #################################################################
 import tensorflow as tf
 x = tf.placeholder(tf.float32, [5, 90000])
-# convolution, flatten
+
 W = tf.Variable(tf.zeros([90000, 2]))
 b = tf.Variable(tf.zeros([5, 2]))
 
-# print(b.shape)
-
-
-# In[17]:
 
 y = tf.matmul(x, W) + b
 y_ = tf.placeholder(tf.int32, [5])
@@ -94,36 +89,21 @@ init = tf.initialize_all_variables()
 sess.run(init)
 
 
-# In[26]:
 
+for i in range(200):
+    # print("batch num", i)
+    if i%4 == 3:
+        continue
+    t, bruce, prediction, c, error = sess.run([train_step, W, pred, cross_entropy, err], feed_dict={x: data[(i%4)*5:((i+1)%4)*5], y_: lab[(i%4)*5:((i+1)%4)*5]})
 
-
-
-# In[22]:
-batch_1 = [all_imgs[0][0], all_imgs[11][0], all_imgs[2][0], all_imgs[13][0], all_imgs[14][0]]
-batch_2 = [all_imgs[10][0], all_imgs[1][0], all_imgs[12][0], all_imgs[3][0], all_imgs[4][0]]
-label_1 = [1,0,1,0,0]
-label_2 = [0,1,0,1,1]
-for i in range(150):
-    print("batch num", i)
-    if i%2:
-        t, bruce, prediction, c, error = sess.run([train_step, W, pred, cross_entropy, err], feed_dict={x: batch_1, y_: label_1})
-        print("Cross Entropy",error)
-    else:
-        t, bruce, prediction, c, error = sess.run([train_step, W, pred, cross_entropy, err], feed_dict={x: batch_2, y_: label_2})
-        print("Cross Entropy",error)
-    
-    
     
 
 
-# In[19]:
-test_data = [all_imgs[6][0], all_imgs[15][0], all_imgs[8][0], all_imgs[16][0], all_imgs[7][0]]
-test_labels = [1, 0, 1, 0, 1]
 
 # correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
 # accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-print(sess.run([pred], feed_dict={x: test_data, y_: test_labels}))
+print("predicted", sess.run([pred], feed_dict={x: data[15:], y_: lab[15:]}))
+print("correct", lab[15:])
 
 
 
