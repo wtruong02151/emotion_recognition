bacde75064af1454a3c4ba0813021e51b47f3d03
Bruce Nguyen <brucenguyen@Bruces-MacBook-Pro.local>
2016-11-12 16:22:41 -0500

    commented to make code understandable

--- test-w-images.py
+++ test-w-images.py
@@ -1,73 +1,60 @@
 
-# coding: utf-8
-
-# In[2]:
-
 # Typical setup to include TensorFlow.
 import tensorflow as tf
 import numpy as np
 
-
-# In[16]:
-
+##################################### READ IN DATA #####################################
 # Make a queue of file names including all the JPEG images files in the relative
 # image directory.
-
-
 filename_queue = tf.train.string_input_producer(
     tf.train.match_filenames_once('undercooked/*.jpg'))
 
-fi = tf.train.match_filenames_once('undercooked/*.jpg')
+# Used to read the entire file
+file_reader = tf.WholeFileReader()
 
-# filenames = ['undercooked/green.jpg', 'undercooked/blue.jpg']
-# filename_queue = tf.train.string_input_producer(filenames)
+# Read a whole file from the queue. Not entirely sure how this works but it returns 
+# a key and value, that we can use to iterate through the entire file.
+key, value = file_reader.read(filename_queue)
 
-# Read an entire image file which is required since they're JPEGs, if the images
-# are too large they could be split in advance to smaller files or use the Fixed
-# reader to split up the file.
-image_reader = tf.WholeFileReader()
-
-# Read a whole file from the queue, the first returned value in the tuple is the
-# filename which we are ignoring.
-key, value = image_reader.read(filename_queue)
-# print value
-
-
-
-# Decode the image as a JPEG file, this will turn it into a Tensor which we can
-# then use in training.
-# my_image = tf.image.decode_jpeg(value, channels=1)
+#Array to hold our image data and also our labels
 data = []
 lab = []
 
-seen = set()
-# Start a new session to show example output.
+
 with tf.Session() as sess:  
-    # Required to get the filename matching to run.
+
     tf.initialize_all_variables().run()
 
     # Coordinate the loading of image files.
     coord = tf.train.Coordinator()
     threads = tf.train.start_queue_runners(coord=coord)
     
+    # Right now there are 20 images in our file "uncooked"
     for i in range(20):
+        # I think what happens when you call eval on either key or value, it will iterate both key and 
+        # val to the next item in the queue. I tried calling key.eval and value.eval both and that fucked it up.
+        # Key here is the file's name. Value is actually the file.
         file_name = key.eval()
         labels = file_name.split('-')
+        if labels[1] == 'AM': #'AM' stands for Asian Male, but I only have AM and AF currently in the file
+            lab.append(1)
+        else: # 0 for girls
+            lab.append(0)
+
+        # Read the in the image and decode it. It might be possible here to use "value" instead of,
+        # but I'm not sure. You guys can mess with it.
         my_img =  tf.image.decode_jpeg(tf.read_file(file_name), channels=1)
         image = tf.image.resize_images(my_img, 300, 300)
         leh = tf.reshape(image, [1, 90000])
-        if labels[1] == 'AM':
-            lab.append(1)
-        else:
-            lab.append(0)
         data.append(leh.eval()[0])
+
     coord.request_stop()
     coord.join(threads)
   
 
 
-#################################################################
-import tensorflow as tf
+##################################### TRAIN AND TEST #####################################
+
 x = tf.placeholder(tf.float32, [5, 90000])
 
 W = tf.Variable(tf.zeros([90000, 2]))
@@ -76,7 +63,6 @@ b = tf.Variable(tf.zeros([5, 2]))
 
 y = tf.matmul(x, W) + b
 y_ = tf.placeholder(tf.int32, [5])
-
 
 pred = tf.argmax(y, 1)
 
@@ -89,21 +75,23 @@ init = tf.initialize_all_variables()
 sess.run(init)
 
 
+#Weird indexing to make it train repeatedly on the first 15 datapts. The 200 batches here is arbitrary
 
+#You'll notice sometimes it learns a lot better than other times. Probably due to random initialization of weights
 for i in range(200):
-    # print("batch num", i)
+    #For indexing purposes
     if i%4 == 3:
         continue
     t, bruce, prediction, c, error = sess.run([train_step, W, pred, cross_entropy, err], feed_dict={x: data[(i%4)*5:((i+1)%4)*5], y_: lab[(i%4)*5:((i+1)%4)*5]})
-
     
-
-
-
-# correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
-# accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-print("predicted", sess.run([pred], feed_dict={x: data[15:], y_: lab[15:]}))
-print("correct", lab[15:])
+    if i%30 == 0:
+        print "batch", i
+        print "error", error
+        print "------------"
+    
+#Test on the last 5 datapts. 
+print "predicted", sess.run(pred, feed_dict={x: data[15:], y_: lab[15:]})
+print "correct", lab[15:]
 
 
 
