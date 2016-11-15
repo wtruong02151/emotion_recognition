bc5348894f6e028e2a16682bbbf152319ede549f
hyeddana <hnyeddanapudy@hotmail.com>
2016-11-15 17:27:46 -0500

    CNN but params are off

--- test-w-images.py
+++ test-w-images.py
@@ -3,6 +3,22 @@
 import tensorflow as tf
 import numpy as np
 
+
+def weight_variable(shape):
+  initial = tf.truncated_normal(shape, stddev=0.1)
+  return tf.Variable(initial)
+
+def bias_variable(shape):
+  initial = tf.constant(0.1, shape=shape)
+  return tf.Variable(initial)
+
+def conv2d(x, W):
+  return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
+
+def max_pool_2x2(x):
+  return tf.nn.max_pool(x, ksize=[1, 2, 2, 1],
+                        strides=[1, 2, 2, 1], padding='SAME')
+
 ##################################### READ IN DATA #####################################
 
 # Make a queue of file names including all the JPEG images files in the relative
@@ -79,20 +95,56 @@ BATCHES = int(len(train_data)/BATCHSZ)
 ##################################### BUILD THE MODEL #####################################
 
 x = tf.placeholder(tf.float32, [BATCHSZ, 90000])
+W_conv1 = weight_variable([5, 5, 1, 32])
+b_conv1 = bias_variable([32])
 
-W = tf.Variable(tf.zeros([90000, 2]))
-b = tf.Variable(tf.zeros([BATCHSZ, 2]))
+x_image = tf.reshape(x, [-1,300,300,1])
+
+h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
+h_pool1 = max_pool_2x2(h_conv1)
+
+W_conv2 = weight_variable([5, 5, 32, 64])
+b_conv2 = bias_variable([64])
+
+h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
+h_pool2 = max_pool_2x2(h_conv2)
+
+W_fc1 = weight_variable([7 * 7 * 64, 4])
+b_fc1 = bias_variable([4])
+
+h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*64])
+h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
+
+keep_prob = tf.placeholder(tf.float32)
+h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
+
+W_fc2 = weight_variable([4, 2]) # 2nd param is number of labels we have
+b_fc2 = bias_variable([2]) # 2nd param is number of labels we have
+
+y_conv=tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)
 
 
-y = tf.matmul(x, W) + b
-y_ = tf.placeholder(tf.int32, [BATCHSZ])
+cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y_conv), reduction_indices=[1]))
+train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
+pred = tf.equal(tf.argmax(y_conv,1), tf.argmax(y_,1))
+err = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
-pred = tf.argmax(y, 1)
+## NON CNN
+# x = tf.placeholder(tf.float32, [BATCHSZ, 90000])
 
-cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(y, y_)
+# W = tf.Variable(tf.zeros([90000, 2]))
+# b = tf.Variable(tf.zeros([BATCHSZ, 2]))
 
-err  = tf.reduce_mean(cross_entropy)
-train_step = tf.train.GradientDescentOptimizer(1e-4).minimize(err)
+
+# y = tf.matmul(x, W) + b
+# y_ = tf.placeholder(tf.int32, [BATCHSZ])
+
+# pred = tf.argmax(y, 1)
+
+# cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(y, y_)
+
+# err  = tf.reduce_mean(cross_entropy)
+# train_step = tf.train.GradientDescentOptimizer(1e-4).minimize(err)
 
 ##################################### TEST AND TRAIN #####################################
 
